7f2d7d0b34c7d6115b31f47a42fbce548a75a6bc
danieltendet <daniel.tendet@student.moringaschool.com>
2021-09-08 12:35:31 +0300

    added formular for calculating which day of the week the user is born

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+akanForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    gender = document.getElementById("gender").value
+    dateOfBirth = document.getElementById("date").value
+
+    dateOfBirth = new Date(dateOfBirth);
+
+    let yearOfBirth = dateOfBirth.getFullYear();
+    let birthMonth = dateOfBirth.getMonth() + 1;
+    let dayOfBirth = dateOfBirth.getDate();
+
+    CC = yearOfBirth.toString().substr(0, 2);
+    YY = yearOfBirth.toString().substr(2, 2);
+    MM = birthMonth
+    DD = dayOfBirth
+
+    dayOfTheWeek = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
+
+    dayOfTheWeek = parseInt(dayOfTheWeek);
